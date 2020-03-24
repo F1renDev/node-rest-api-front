2dc9c4b19188bc5831a5f0c17cb4b54345a6d038
F1renDev <F1renishere@gmail.com>
2020-03-24 12:03:45 +0300

    Switched to hooks

--- src/pages/Feed/SinglePost/SinglePost.js
+++ src/pages/Feed/SinglePost/SinglePost.js
@@ -1,19 +1,71 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
+
+// import Image from "../../../components/Image/Image";
+// import "./SinglePost.css";
+
+// class SinglePost extends Component {
+//   state = {
+//     title: "",
+//     author: "",
+//     date: "",
+//     image: "",
+//     content: ""
+//   };
+
+//   componentDidMount() {
+//     const postId = this.props.match.params.postId;
+//     fetch("URL")
+//       .then(res => {
+//         if (res.status !== 200) {
+//           throw new Error("Failed to fetch status");
+//         }
+//         return res.json();
+//       })
+//       .then(resData => {
+//         this.setState({
+//           title: resData.post.title,
+//           author: resData.post.creator.name,
+//           date: new Date(resData.post.createdAt).toLocaleDateString("en-US"),
+//           content: resData.post.content
+//         });
+//       })
+//       .catch(err => {
+//         console.log(err);
+//       });
+//   }
+
+//   render() {
+//     return (
+//       <section className="single-post">
+//         <h1>{this.state.title}</h1>
+//         <h2>
+//           Created by {this.state.author} on {this.state.date}
+//         </h2>
+//         <div className="single-post__image">
+//           <Image contain imageUrl={this.state.image} />
+//         </div>
+//         <p>{this.state.content}</p>
+//       </section>
+//     );
+//   }
+// }
+
+// export default SinglePost;
+
+import React, { useState, useEffect } from "react";
 
 import Image from "../../../components/Image/Image";
 import "./SinglePost.css";
 
-class SinglePost extends Component {
-  state = {
-    title: "",
-    author: "",
-    date: "",
-    image: "",
-    content: ""
-  };
+const SinglePost = props => {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [date, setDate] = useState("");
+  const [image, setImage] = useState("");
+  const [content, setContent] = useState("");
 
-  componentDidMount() {
-    const postId = this.props.match.params.postId;
+  useEffect(() => {
+    const postId = props.match.params.postId;
     fetch("URL")
       .then(res => {
         if (res.status !== 200) {
@@ -22,32 +74,28 @@ class SinglePost extends Component {
         return res.json();
       })
       .then(resData => {
-        this.setState({
-          title: resData.post.title,
-          author: resData.post.creator.name,
-          date: new Date(resData.post.createdAt).toLocaleDateString("en-US"),
-          content: resData.post.content
-        });
+        setTitle(resData.post.title);
+        setAuthor(resData.post.creator.name);
+        setDate(new Date(resData.post.createdAt).toLocaleDateString("en-US"));
+        setContent(resData.post.content);
       })
       .catch(err => {
         console.log(err);
       });
-  }
+  });
 
-  render() {
-    return (
-      <section className="single-post">
-        <h1>{this.state.title}</h1>
-        <h2>
-          Created by {this.state.author} on {this.state.date}
-        </h2>
-        <div className="single-post__image">
-          <Image contain imageUrl={this.state.image} />
-        </div>
-        <p>{this.state.content}</p>
-      </section>
-    );
-  }
-}
+  return (
+    <section className="single-post">
+      <h1>{title}</h1>
+      <h2>
+        Created by {author} on {date}
+      </h2>
+      <div className="single-post__image">
+        <Image contain imageUrl={image} />
+      </div>
+      <p>{content}</p>
+    </section>
+  );
+};
 
 export default SinglePost;
